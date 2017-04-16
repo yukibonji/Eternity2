d7e300a3e834b20eb39c76531a5db27967484347
Nick Palladinos <nick.palladinos@gmail.com>
2017-04-16 20:03:44 +0300

    Basic type and operators.

--- src/Eternity2/Script.fsx
+++ src/Eternity2/Script.fsx
@@ -1,1 +1,34 @@
-﻿
+﻿#time
+
+// Basic type for propositional logic formulas
+type BoolExpr = 
+    | Or of BoolExpr * BoolExpr
+    | And of BoolExpr * BoolExpr
+    | Not of BoolExpr
+    | Var of string
+    | True 
+    | False
+
+// Basic operators
+let (<&>) a b =
+    And (a, b)
+
+let (<|>) a b =
+    Or (a, b)
+
+let (~~) a = 
+    Not a 
+
+let (=>) a b =
+    Or (a, Not b)
+
+let (<=>) a b = 
+    And (a => b, b => a)
+
+let var a =
+    Var a
+
+let tt = True
+let ff = False
+
+
