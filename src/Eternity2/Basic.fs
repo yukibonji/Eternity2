cbfba3526817e0ea7faed3f296b67b6b4d53d412
Nick Palladinos <nick.palladinos@gmail.com>
2017-04-16 20:27:45 +0300

    Add simple normalization function and refactor prj structure.

---
+++ src/Eternity2/Basic.fs
@@ -0,0 +1,63 @@
+﻿namespace Eternity2
+
+module Basic = 
+    // Basic type for propositional logic formulas
+    type BoolExpr = 
+        | Or of BoolExpr * BoolExpr
+        | And of BoolExpr * BoolExpr
+        | Not of BoolExpr
+        | Var of string
+        | True 
+        | False
+
+    // Basic operators
+    let (<&>) a b =
+        And (a, b)
+
+    let (<|>) a b =
+        Or (a, b)
+
+    let (~~) a = 
+        Not a 
+
+    let (=>) a b =
+        Or (a, Not b)
+
+    let (<=>) a b = 
+        And (a => b, b => a)
+
+    let var a =
+        Var a
+
+    let tt = True
+    let ff = False
+
+
+    // Transformations
+    let rec deMorgan expr = 
+        match expr with 
+        | Not (Not expr') -> expr'
+
+        | Not (Or (first, second)) -> And (Not first, Not second) 
+        | Not (And (first, second)) -> Or (Not first, Not second) 
+
+        | And (first, second) -> And (deMorgan first, deMorgan second)
+        | Or (first, second) -> Or (deMorgan first, deMorgan second)
+        | Not expr' -> Not (deMorgan expr')
+        | Var _ | True | False -> expr
+
+    let rec distributive expr = 
+        match expr with
+        | Or (push, And (first, second)) -> And (Or (push, first), Or (push, second)) 
+        | Or (And (first, second), push) -> And (Or (push, first), Or (push, second)) 
+
+        | And (first, second) -> And (distributive first, distributive second)
+        | Or (first, second) -> Or (distributive first, distributive second)
+        | Not expr' -> Not (distributive expr')
+        | Var _ | True | False -> expr
+
+
+
+    let rec normalize expr = 
+        let expr' = distributive (deMorgan expr)
+        if expr = expr' then expr' else normalize expr'
--- src/Eternity2/Program.fs
+++ src/Eternity2/Program.fs
@@ -1,7 +1,8 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿namespace Eternity2
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+module Program = 
+
+    [<EntryPoint>]
+    let main argv = 
+        printfn "%A" argv
+        0 // return an integer exit code
