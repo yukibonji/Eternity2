f0df842e0a182fa15a9a4b244fa56e64628e7a2e
Nick Palladinos <nick.palladinos@gmail.com>
2017-04-16 20:14:35 +0300

    Implement deMorgan, distributive transformations.

--- src/Eternity2/Script.fsx
+++ src/Eternity2/Script.fsx
@@ -32,3 +32,25 @@ let tt = True
 let ff = False
 
 
+// Transformations
+let rec deMorgan expr = 
+    match expr with 
+    | Not (Not expr') -> expr'
+
+    | Not (Or (first, second)) -> And (Not first, Not second) 
+    | Not (And (first, second)) -> Or (Not first, Not second) 
+
+    | And (first, second) -> And (deMorgan first, deMorgan second)
+    | Or (first, second) -> Or (deMorgan first, deMorgan second)
+    | Not expr' -> Not (deMorgan expr')
+    | Var _ | True | False -> expr
+
+let rec distributive expr = 
+    match expr with
+    | Or (push, And (first, second)) -> And (Or (push, first), Or (push, second)) 
+    | Or (And (first, second), push) -> And (Or (push, first), Or (push, second)) 
+
+    | And (first, second) -> And (distributive first, distributive second)
+    | Or (first, second) -> Or (distributive first, distributive second)
+    | Not expr' -> Not (distributive expr')
+    | Var _ | True | False -> expr
